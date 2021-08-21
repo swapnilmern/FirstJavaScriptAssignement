46102a41d4fb7e38fb5da87a7fc3d5452bb0f91a
swapnil <swapnil.mern@gmail.com>
2021-08-21 10:19:39 +0530

    First HTML and JS files has been added.

---
+++ practice.js
@@ -0,0 +1,48 @@
+// let admin,Name;
+// Name = 'John';
+// admin = Name;// copy value of Name into admin
+// admin = 'Name';// assign Name to admin
+// console.log("Varaibale Name value is==",admin );
+// alert("Copied Admin:"+admin);
+// // 10,30,'Swapnil',
+// let myFloat= 10.20;
+// admin = myFloat;
+
+// let a = 10;
+// let b=20;
+// alert (a+b);
+// let userName = 'John';
+
+// function showMessage() {
+//   let message = 'Hello, ' + userName; 'Hello, ' + 'John'
+//   alert(message);
+// }
+
+// showMessage(); 
+console.log('Debugging 1.0')
+function checkAge(age) {
+    console.log('Debugging 2.0')
+    if (age >= 18) {
+      return true;
+    } else {
+      return confirm('Do you have permission from your parents?');
+    }
+  }
+  console.log('Debugging 3.0')
+let xyz = 1000;
+const MYCOST= 'NAME';
+console.log('Debugging 4.0') 
+let age = prompt('How old are you?',20);
+console.log('Debugging 5.0')   
+  
+if ( checkAge(age) ) {
+    console.log('Debugging 6.0') 
+    alert( 'Access granted'+ xyz );
+  } else {
+    console.log('Debugging 7.0') 
+    alert( 'Access denied'+ MYCOST );
+  }
+
+  console.log('Debugging 8.0') 
+
+
